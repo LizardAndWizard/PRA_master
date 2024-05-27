591f8147e1a902258293ea32875a1f0449317ef0
pra2-admin <tbogdan@algebra.hr>
2024-05-27 11:42:26 +0200

    Added ReviewController with action Add. Changed JWT secure key to a more secure one.

---
+++ DrivingSchoolPRA/app/Controllers/ReviewController.cs
@@ -0,0 +1,48 @@
+﻿using app.DTOs;
+using app.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private PraDrivingSchoolContext _context;
+
+        public ReviewController(PraDrivingSchoolContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public ActionResult Add([FromBody]ReviewDto reviewDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var review = new Review
+                {
+                    StudentId = reviewDto.StudentOIB,
+                    InstructorId = reviewDto.InstructorId,
+                    Grade = reviewDto.Grade,
+                    Comment = reviewDto.Comment,
+                };
+
+                _context.Add(review);
+                _context.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ DrivingSchoolPRA/app/DTOs/ReviewDto.cs
@@ -0,0 +1,21 @@
+﻿using app.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace app.DTOs
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The {0} is required.")]
+        public string StudentOIB { get; set; } = null!;
+
+        [Required(ErrorMessage = "The {0} is required.")]
+        public int InstructorId { get; set; }
+
+        [Required(ErrorMessage = "The {0} is required.")]
+        public int Grade { get; set; }
+
+        public string? Comment { get; set; }
+    }
+}
